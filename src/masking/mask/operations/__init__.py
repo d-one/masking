f6f31fa23ffbf44728fd82916f94bd1667a96f70
Marco Petrella <marco.petrella@d-one.ai>
2024-07-29 10:46:49 +0200

    Adding lazy load

--- src/masking/mask/operations/__init__.py
+++ src/masking/mask/operations/__init__.py
@@ -1,11 +1,33 @@
-from .operation_date import FakeDate
-from .operation_hash import HashSHA256, Operation
-from .operation_plz import FakePLZ
-from .operation_presidio import HashPresidio
+from .operation import Operation
+
+
+def load_fake_date() -> Operation:
+    from .operation_date import FakeDate
+
+    return FakeDate
+
+
+def load_fake_plz() -> Operation:
+    from .operation_plz import FakePLZ
+
+    return FakePLZ
+
+
+def load_hash_presidio() -> Operation:
+    from .operation_presidio import HashPresidio
+
+    return HashPresidio
+
+
+def load_hash_sha256() -> Operation:
+    from .operation_hash import HashSHA256
+
+    return HashSHA256
+
 
 MASKING_OPERATIONS: dict[str, Operation] = {
-    "hash": HashSHA256,
-    "presidio": HashPresidio,
-    "fake_date": FakeDate,
-    "fake_plz": FakePLZ,
+    "hash": load_hash_sha256(),
+    "presidio": load_hash_presidio(),
+    "fake_date": load_fake_date(),
+    "fake_plz": load_fake_plz(),
 }
